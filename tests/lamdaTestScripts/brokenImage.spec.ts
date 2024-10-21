818098635f9abb353329109b366e6b0e89a2ccf3
sabarishkv <veerasabarish.k@gmail.com>
2024-10-21 11:12:43 +0530

    Added faker

--- tests/lamdaTestScripts/brokenImage.spec.ts
+++ tests/lamdaTestScripts/brokenImage.spec.ts
@@ -1,13 +1,24 @@
 import { test } from "@playwright/test";
-
+import {faker} from '@faker-js/faker'
+import { LargeNumberLike } from "crypto";
+import { an } from "@faker-js/faker/dist/airline-C5Qwd7_q";
 
 //Sources:
-//https://www.youtube.com/watch?v=4G8c7BwHY5s&ab_channel=LetCodewithKoushik 
+//https://www.youtube.com/watch?v=4G8c7BwHY5s&ab_channel=LetCodewithKoushik
 
 test.describe("Testing the broken image Test Case", () => {
   test("Testing the broken image", async ({ page }) => {
-    await page.goto(
-      "https://www.lambdatest.com/selenium-playground/broken-image"
-    );
+    await test.step("Launching the Application", async () => {
+      await page.goto(
+        "https://www.lambdatest.com/selenium-playground/broken-image"
+      );
+    });
+
+    await test.step("Find the broken image link", async () => {
+
+        let name: string =  faker.person.fullName({sex:'male'});
+
+        console.log(name);
+    });
   });
 });
